8f4160e7f2096b4917cca921f74bacf9a77a7191
Antonio <ajpelu@gmail.com>
2017-04-27 17:01:05 +0200

    update analysis

--- script/R/rw_byTree.R
+++ script/R/rw_byTree.R
@@ -1,21 +1,21 @@
-# Function rwl by tree
+# Name: rw_byTree
 
-# Compute rwl mean by tree
+# Compute rw mean by tree
 # This function needs a dataframe with the ring width measurements (a dataframe of the class 'rwl' from dplR pkg) 
 # It creates a new dataframe with the average value of the ring width (RW) by tree; and also a varible with the year. 
 # Tree names coming from colnames of the dataframe (see from class::rwl). The colnames of this rwl have the 
 # following structure: SNA0101 (SNA|id_tree|id_core). 
  
-rwl_byTree <- function(rwldf){ 
+rw_byTree <- function(rwdf){ 
   require(dplyr)
   
   # Create output with rownmaes (year) as a variable 
-  output_avg <- rwldf %>% 
+  output_avg <- rwdf %>% 
     mutate(year = as.numeric(rownames(.))) %>% 
     dplyr::select(year)
   
   # Get name of each tree (each serie: i.e.:  SNA0101 (SNA|id_tree|id_core)) 
-  tree_names <- unique(stringr::str_sub(names(rwldf), start=1, end=5))
+  tree_names <- unique(stringr::str_sub(names(rwdf), start=1, end=5))
   
   # Loop 
   for (i in tree_names) {
@@ -23,7 +23,7 @@ rwl_byTree <- function(rwldf){
     name_avg <- paste0('rw_',stringr::str_sub(i, star=3, end=5))
     
     # Create aux dataframe with rwl mean values and the year
-    aux <- rwldf %>% 
+    aux <- rwdf %>% 
       dplyr::select(starts_with(i)) %>% 
       mutate_(.dots = setNames(list(~rowMeans(., na.rm=TRUE),
                                     ~as.numeric(rownames(.))),
