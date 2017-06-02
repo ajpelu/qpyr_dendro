38a534833a62564438ece9d67a1b5ce0e03b0aa7
Antonio <ajpelu@gmail.com>
2017-06-02 15:17:08 +0200

    update median computation

--- script/R/computeGC.R
+++ script/R/computeGC.R
@@ -36,15 +36,28 @@ computeGC <- function(dfrwl, ws, prefijo){
       # mean values
       mean_m2 <- mean(m2[,1])
       mean_m1 <- mean(m1[,1])
+      
+      # median 
+      median_m2 <- median(m2[,1])
+      median_m1 <- median(m1[,1])
+      
       # GC 
-      pgc = ((mean_m2 - mean_m1) / mean_m1) * 100 
-      ngc = ((mean_m1 - mean_m2) / mean_m2) * 100 
+      pgc_m = ((mean_m2 - mean_m1) / mean_m1) * 100 
+      ngc_m = ((mean_m1 - mean_m2) / mean_m2) * 100 
+      
+      # 
+      # GC 
+      pgc_md = ((median_m2 - median_m1) / median_m1) * 100 
+      ngc_md = ((median_m1 - median_m2) / median_m2) * 100 
       
       # out 
-      gc_aux <- data.frame(pgc = pgc, ngc =ngc,
+      gc_aux <- data.frame(pgc = pgc_m, ngc = ngc_m,
+                           pgc_md = pgc_md, ngc_md = ngc_md,
                            year = y, name_serie = ns)
       
-      gc_aux$gc <- ifelse(gc_aux$pgc > 0, gc_aux$pgc, (-1)*gc_aux$ngc)
+      gc_aux$gc_mean <- ifelse(gc_aux$pgc > 0, gc_aux$pgc, (-1)*gc_aux$ngc)
+      gc_aux$gc_median <- ifelse(gc_aux$pgc_md > 0, gc_aux$pgc_md, (-1)*gc_aux$ngc_md)
+      
       gc_aux$tree <- as.numeric(stringr::str_replace(gc_aux$name_serie, prefijo, ""))
       
       aux_serie <- rbind(aux_serie, gc_aux)
